1457de9e1f2d9f98f9f8ff7822006f90f74a5191
Xhevair Llakaj <xhllakaj@gmail.com>
2021-11-11 16:16:58 +0100

    clean code

--- lang/views.py
+++ lang/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.utils.translation import gettext as _
 from django.utils.translation import get_language, activate, gettext
-# from .models import trans_texts, translate
 
 # using getext as _
 def home(request):

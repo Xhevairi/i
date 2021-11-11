955d844f9026271367b79116fc2c6baa0de50080
Xhevair Llakaj <xhllakaj@gmail.com>
2021-11-11 16:15:36 +0100

    clean code

--- lang/views.py
+++ lang/views.py
@@ -6,34 +6,19 @@ from django.utils.translation import get_language, activate, gettext
 # using getext as _
 def home(request):
     trans = _('hello')
-    # using translate() func
+    # using translate func
     # trans = translate(language = 'fr')
     return render(request, 'lang/home.html', {'trans': trans})
 
 def item(request):
     return render(request, 'lang/item.html')
 
-# # texts in default language
-# def trans_texts():
-#     texts = (
-#             'Hello, '
-#             'hello',
-#             'Language',
-#             'home',
-#             'item',
-#             'In this Python Django tutorial I will take you through the basics of setting up internationalization in Django so that you your.',
-#             'Python Django - Multiple Languages - Internationalization - Part 1'
-#         )
-#     return texts 
-
-# # translate texts
+# # translate words
 # def translate(language):
 #     cur_language = get_language()
 #     try:
 #         activate(language)
-#         texts = trans_texts()
-#         for text in texts:
-#             text = gettext(text)
+#         text = gettext('hello')
 #     finally:
 #         activate(cur_language)
 #     return text
